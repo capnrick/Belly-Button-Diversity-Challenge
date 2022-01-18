34e1983ed781c09e43542e2b44ecf7d1d5998802
capnrick <herrric@hawk.iit.edu>
2022-01-17 20:00:15 -0600

    Added funciton stubs and an event handler.

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,6 +1,41 @@
 console.log("This is plots.js")
 
 
+function optionChanged(id)
+
+{
+    console.log(`optionChanged(${id})`);
+    
+    DrawBarchart(id);
+    DrawBubbleChart(id);
+    showMetadata(id);
+
+}
+
+
+//function stubs for each required chart/table
+function DrawBarchart(sampleId){
+
+    console.log(`Drawbarchart(${sampleId})`);
+
+    //define function here
+
+
+
+}
+ 
+
+function DrawBubbleChart(sampleId){
+
+    console.log(`DrawBubblechart (${sampleId})`);
+}
+
+function showMetadata(sampleId){
+
+    console.log(`showMetadata(${sampleId})`);
+}
+
+
 //this code taken from Dom's office hours session Dec 11th.
 function InitDashboard()
 {
@@ -16,17 +51,35 @@ function InitDashboard()
         //take a look at verify the data is being read
         console.log(data);
 
-        //define object of names
+        //define object of only names
         let sampleNames=data.names;
 
+        //in the array of names, add this
         sampleNames.forEach(sampleId=>{
             selector.append("option")
                     .text(sampleId)
                     .property("value",sampleId);
-
         });
+
+        //initializing dashboard with first value in array from JSONS
+        let sampleId=sampleNames[0];
+
+        console.log("this is the sample Id at init")
+        console.log(sampleId)
+        DrawBarchart(sampleId);
+        DrawBubbleChart(sampleId);
+        showMetadata(sampleId);
+
+
+
     }); 
 
 }
 
-InitDashboard();
+
+
+
+
+
+InitDashboard(); 
+
